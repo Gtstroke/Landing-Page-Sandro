08e1f2d66bf4a2e72c7a9371b70e70ed3d00b028
Gtstroke <142357932+Gtstroke@users.noreply.github.com>
2024-09-19 21:37:18 -0300

    Primeiro commit

---
+++ src/components/Rodape/Rodape.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Rodape.css';
+
+function Rodape() {
+    return (
+        <footer className="rodape">
+            <div className='rodape-conteudo'>
+                <p>&copy; 2023 Clube de Regatas do Flamengo. Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Rodape;
